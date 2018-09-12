77d02a66fb304a6e17c7fcfe078e8a39c91dc1f6
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-12 12:16:49 -0400

    validations finally fixed

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -5,7 +5,7 @@ class Instructor < ApplicationRecord
     validates :age, :inclusion => {:in => 1..149, :message => "should be between 1 and 149"}
     validates_presence_of   :first_name
     validates_presence_of   :last_name
-    validates :salary, :inclusion => {:in => 1..1000000, , :message => "should be between 1 and 1,000,000"}
+    validates :salary, :inclusion => {:in => 1..1000000, :message => "should be between 1 and 1,000,000"}
 
     def full_name
     self.first_name + " " + self.last_name
