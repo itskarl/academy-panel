164686c276035fcbe1c49b561161de9896625860
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-12 11:57:53 -0400

    aaa

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -7,6 +7,4 @@ class Cohort < ApplicationRecord
   validates_presence_of   :start_date
   validates_presence_of   :end_date
 
-
-
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
   has_many :cohorts, dependent: :destroy
 
-  validates :hours, :inclusion => {:in => 1..400, :message => "should be between 1 and 400"}
+  # validates :hours, :inclusion => {:in => 1..400, :message => "should be between 1 and 400"}
 end
